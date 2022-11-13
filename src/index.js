9ae8274c8ba8863c908dc4f1ec9c42e27f87ab44
CubE135 <gitlab@cub3mail.de>
2022-11-13 21:16:11 +0100

    🐛 Disable Dev Mode

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 app.on('ready', createWindow);
